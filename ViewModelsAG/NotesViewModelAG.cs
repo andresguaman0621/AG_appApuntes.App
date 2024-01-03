60ba9ceded8ee212b604c34b6061ec7782b79774
Andres Alejandro Guaman Taco <andres.guaman.taco@udla.edu.ec>
2024-01-02 21:22:57 -0500

    Creacion del modelo Vista Notas

---
+++ ViewModelsAG/NotesViewModelAG.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using AG_appApuntes.Models;
+
+
+namespace AG_appApuntes.ViewModelsAG
+{
+    internal class NotesViewModelAG : IQueryAttributable
+    {
+        public ObservableCollection<ViewModelsAG.NoteViewModelAG> AllNotes { get; }
+        public ICommand NewCommand { get; }
+        public ICommand SelectNoteCommand { get; }
+
+        public NotesViewModelAG()
+        {
+            AllNotes = new ObservableCollection<ViewModelsAG.NoteViewModelAG>(Models.Note.LoadAll().Select(n => new NoteViewModelAG(n)));
+            NewCommand = new AsyncRelayCommand(NewNoteAsync);
+            SelectNoteCommand = new AsyncRelayCommand<ViewModelsAG.NoteViewModelAG>(SelectNoteAsync);
+        }
+
+        private async Task NewNoteAsync()
+        {
+            await Shell.Current.GoToAsync(nameof(Views.AG_NotePage));
+        }
+
+        private async Task SelectNoteAsync(ViewModelsAG.NoteViewModelAG note)
+        {
+            if (note != null)
+                await Shell.Current.GoToAsync($"{nameof(Views.AG_NotePage)}?load={note.Identifier}");
+        }
+
+        void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            if (query.ContainsKey("deleted"))
+            {
+                string noteId = query["deleted"].ToString();
+                NoteViewModelAG matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+                // If note exists, delete it
+                if (matchedNote != null)
+                    AllNotes.Remove(matchedNote);
+            }
+            else if (query.ContainsKey("saved"))
+            {
+                string noteId = query["saved"].ToString();
+                NoteViewModelAG matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+                // If note is found, update it
+                if (matchedNote != null)
+                    matchedNote.Reload();
+
+                // If note isn't found, it's new; add it.
+                else
+                    AllNotes.Add(new NoteViewModelAG(Note.Load(noteId)));
+            }
+        }
+    }
+}
--- Views/AG_AboutPage.xaml.cs
+++ Views/AG_AboutPage.xaml.cs
@@ -7,11 +7,5 @@ public partial class AG_AboutPage : ContentPage
 		InitializeComponent();
 	}
     
-    private async void LearnMore_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.About about)
-        {
-            await Launcher.Default.OpenAsync("https://aka.ms/maui");
-        }
-    }
+    
 }
